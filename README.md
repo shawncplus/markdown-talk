2f7a5873633a020b9fd894d390984443acf1e7fc
Shawn Biddle <shawncplus@gmail.com>
2014-04-26 19:34:27 -0400

    Initial commit of the markdown-tutorial skeleton

---
+++ README.md
@@ -0,0 +1,9 @@
+Uses a patched version of https://github.com/arturadib/strapdown which supports a 'source' tag, along with some extra features to navigate the tutorial files I tend to generate which are bulleted list outlines of the content.
+
+Features
+* Base styles to make things look _generally_ better than the default slate theme
+* j/k navigation highlighting the current talking point
+* That's pretty much all I need. I mean, it just renders a markdown file pretty-like and lets me display it in a highly readable way for presentations
+
+Acknowledgements
+* https://github.com/arturadib/strapdown
---
+++ markdown-talk.js
@@ -0,0 +1,83 @@
+document.addEventListener('markdownrender', function ()
+{
+	// append the subtitle element to the navbar
+	var subel = document.querySelector('subtitle');
+	if (subel)
+	{
+		var subtitle = document.createElement('div');
+		subtitle.innerHTML = subel.innerHTML;
+		subtitle.classList.add('subtitle');
+		document.querySelector('div.container').appendChild(subtitle);
+	}
+
+	var focusPoints = document.querySelectorAll('ol > li');
+	if (!focusPoints.length)
+	{
+		// no need for the legend if they have nothing to navigate
+		document.querySelector('legend').style.display = 'none';
+		return;
+	}
+
+	var focusIndex = 0;
+	focusPoints[focusIndex].classList.add('selected');
+
+	var hideLegend = false;
+	var spans = document.querySelectorAll('legend > span');
+	var initialLegendWidth = document.querySelector('legend').offsetWidth;
+
+	function toggleLegend(el)
+	{
+		el = el ? el.target : document.querySelector('legend > a');
+		hideLegend = !hideLegend;
+		Array.prototype.map.call(spans, function (span)
+		{
+			span.style.display = hideLegend ? 'none' : '';
+		});
+		el.innerHTML = hideLegend ? '⊞' : '⊟';
+		el.parentNode.style.width = hideLegend ? el.offsetWidth + 'px' : initialLegendWidth + 'px';
+	};
+
+	document.querySelector('legend > a').onclick = toggleLegend;
+
+	var topcontainer = document.querySelector('div.navbar-fixed-top');
+	window.onscroll = function ()
+	{
+		document.querySelector('legend').style.top = Math.max(topcontainer.scrollHeight - scrollY, 0) + 'px';
+	};
+
+	document.onkeydown = function (e)
+	{
+		var keys = [71, 74, 75, 76];
+		if (keys.indexOf(e.keyCode) < 0)
+		{
+			return;
+		}
+
+		if (e.keyCode === 76) // l
+		{
+			return toggleLegend(); 
+		}
+
+		if (e.keyCode === 71) // g
+		{
+			window.scrollTo(0, 0);
+			focusPoints[focusIndex].classList.remove('selected');
+			focusIndex = 0;
+			focusPoints[focusIndex].classList.add('selected');
+			return;
+		}
+
+		var direction = e.keyCode === 74 ? 1 : -1;
+
+		if (focusPoints[focusIndex + direction])
+		{
+			focusPoints[focusIndex].classList.remove('selected');
+			focusIndex += direction;
+			var focusPoint = focusPoints[focusIndex];
+			focusPoint.classList.add('selected');
+
+			var top = focusPoint.offsetTop - ( window.innerHeight / 2 ) + (focusPoint.offsetHeight / 2);
+			window.scrollTo(0, Math.min(top, focusPoint.offsetTop));
+		}
+	};
+});
